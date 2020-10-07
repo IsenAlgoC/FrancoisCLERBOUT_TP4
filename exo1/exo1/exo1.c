d126f546fca776ecaf7a87d5894df88e4aecf78e
Francois CLERBOUT <francois.clerbout@isen.yncrea.fr>
2020-10-07 13:51:50 +0200

    fin exo1

---
+++ exo1/exo1/exo1.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "tp4.h"
+
+
+int main() {
+	HEURE HeureDebut, HeureFin, Duree;
+	HeureDebut.heure = 12; HeureDebut.minute = 30;
+	Duree.heure = 0; Duree.minute = 45;
+
+	HeureFin.minute = (HeureDebut.minute + Duree.minute) % 60;
+	if ((HeureDebut.minute + Duree.minute) > 60) {
+		HeureFin.heure = (HeureDebut.heure + Duree.heure) + 1;
+	}
+	if ((HeureDebut.minute + Duree.minute) < 60){
+		HeureFin.heure = HeureDebut.heure + Duree.heure;
+	}
+
+	printf("HeureDebut = %d:%d\n", HeureDebut.heure, HeureDebut.minute);
+	printf("HeureFin = %d:%d\n", HeureFin.heure, HeureFin.minute);
+	printf("Duree = %d:%d\n", Duree.heure, Duree.minute);
+
+
+
+
+
+
+
+
+
+
+
+
+	return(EXIT_SUCCESS);
+}
---
+++ exo1/exo1/tp4.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//déclaration de la structure heure et du type associé HEURE
+typedef struct heure{ int heure; int minute; } HEURE;
